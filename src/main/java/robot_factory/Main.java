e6ee6c0df4d728d69894bb3e20853b3aac7a143d
kusmangalievasanali <kusmangalievasanali@gmail.com>
2026-09-20 18:48:33 +0500

    Update Main.java

--- src/main/java/robot_factory/Main.java
+++ src/main/java/robot_factory/Main.java
@@ -3,12 +3,24 @@ package robot_factory;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("___PART A: Factory Method___");
+        CoreCreator combatCoreCreator = new CombatCoreCreator();
+        Core combatCore = combatCoreCreator.createCore();
+        combatCore.process();
+
+        CoreCreator domesticCoreCreator = new DomesticCoreCreator();
+        Core domesticCore = domesticCoreCreator.createCore();
+        domesticCore.process();
+
+
+        System.out.println("\n___PART B: Abstract Factory___");
         System.out.println("--- Assembling Combat Robot ---");
         RobotFactory combatFactory = new CombatRobotFactory();
         RobotApplication combatApp = new RobotApplication(combatFactory);
         combatApp.testRobot();
 
-        System.out.println("\n--- Assembling Domestic Robot ---");
+        System.out.println("\n___Assembling Domestic Robot___");
         RobotFactory domesticFactory = new DomesticRobotFactory();
         RobotApplication domesticApp = new RobotApplication(domesticFactory);
         domesticApp.testRobot();
